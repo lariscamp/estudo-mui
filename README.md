9e62142879517f1a53974acefe27c55177baa8e5
Larissa Campelo <larissacampeloalves@gmail.com>
2021-10-22 18:44:21 -0300

    cria form

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-[]instalar biblioteca do @material.ui
-[]criar um form de cadastro
-[]string, boolean e lista de seleção
+[x]instalar biblioteca do @material.ui
+[x]criar repositório no GitHub
+[x]criar um form de cadastro
+[x]string, boolean e lista de seleção
 []ao enviar o form salvar os valores em um json
+[]estilização do form
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,27 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { Grid, Switch, TextField, MenuItem, Select, InputLabel, Button } from '@mui/material'
+import { Box } from '@mui/system'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <main className="App">
+      <Grid container alignItems="center">
+        <Box component="form" sx={{display:"flex", flexDirection:"column", justifyContent:"flex-start", alignItems:"center"}}>
+          <TextField id="nome" required label= "nome" name="nome"/>
+          <TextField id="email" required label= "email" name="email"/>
+          <Switch value="newsletter"/>
+          <InputLabel id="selecao-genero-label">Escolha o gênero que melhor te define:</InputLabel>
+          <Select labelId="selecao-genero-label" id="selecao-genero" label="Gênero">
+            <MenuItem value={"feminino"}>Feminino</MenuItem>
+            <MenuItem value={"masculino"}>Masculino</MenuItem>
+            <MenuItem value={"nb"}>Não Binário</MenuItem>
+            <MenuItem value={"outro"}>Outro</MenuItem>
+          </Select>
+          <Button variant="text" color="primary">Enviar</Button>
+        </Box>    
+      </Grid>  
+    </main>
   )
 }
 
